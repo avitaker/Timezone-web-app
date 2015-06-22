28d591e66f2d5326aff972a5837ca40b8ea4f691
avitaker <avinashdavid@Avinashs-MacBook-Pro.local>
2015-06-22 11:21:21 -0500

    Added select for groups of zones
    No more console errors

--- app.js
+++ app.js
@@ -1,44 +1,68 @@
 var thisApp=angular.module('time_finder',[])
-// .config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
-//   $stateProvider
-//   .state('timezoneSelect',{
-//     url:'/timezoneSelect',
-//     templateUrl:'templates/selectTmpl.html',
-//     controller:'tzSelectCtrl'
-//   })
-//   .state('referencePage',{
-//     url:'/referencePage',
-//     templateUrl:'templates/referenceTmpl.html'
-//   });
-//   $urlRouterProvider.otherwise('/timezoneSelect');
-// }])
 .factory("Data",['$http',function($http){
   var factory={};
   factory.getData=function(){return $http.get('http://localhost:8000/timezones.json')};
   return factory;
 }]).controller("tzSelectCtrl",['$scope','Data','$interval',function($scope,Data,$interval){
+  //Prepare the data here and set initial display
   Data.getData().success(function(arritems){
     $scope.testObj=arritems;
     $scope.test="Your time: ";
   });
+
+  // Narrow down the area if desired
+  var zoneObjDef=function(low,high,area){
+    this.low=low;
+    this.high=high;
+    this.rangeString=low + " to " + high;
+    this.area=area;
+  }
+  $scope.zones=[new zoneObjDef(-12,12,"All timezones"),
+    new zoneObjDef(-12,-10,"IDL to Western Pacific Ocean, Hawaii"),
+    new zoneObjDef(-10,-6,"North America (less East Coast), Central America"),
+    new zoneObjDef(-6,-3,"North America East Coast, South America, Greenland"),
+    new zoneObjDef(-3,0,"Eastern Atlantic Ocean, UK, Northwest Africa"),
+    new zoneObjDef(0,3,"Western and Central Europe (less UK), West Russia, Central and East Africa, Middle East"),
+    new zoneObjDef(3,7,"Eastern Europe, West Central Russia, Middle East, Southeast Asian subcontinent"),
+    new zoneObjDef(7,9,"Central Russia, China and East Asia, Indian Ocean to East Pacific Ocean, West Australia"),
+    new zoneObjDef(9,12,"Eastern Russia, Central and Eastern Australia, East Pacific Ocean to IDL")
+  ];
+  $scope.inRangeTimezones=[];
+  $scope.selectedRangeObj={};
+  $scope.setSelect=function(){
+    $scope.inRangeTimezones=[];
+    if (!$scope.selectedRangeObj.area){
+      $scope.inRangeTimezones=$scope.testObj;
+    }
+    else {
+      for (var i=0;i<$scope.testObj.length;i++){
+        if ($scope.testObj[i].offset>=$scope.selectedRangeObj.low && $scope.testObj[i].offset<$scope.selectedRangeObj.high){
+          $scope.inRangeTimezones.push($scope.testObj[i]);
+        }
+        else {continue};
+      }
+    }
+  }
   $scope.timezoneMapSrc="http://www.satellitecitymaps.com/gfx/map_of_timezones.jpg";
   $scope.showMap=false;
   var nowDate=new Date();
-  var currentOffset=nowDate.getTimezoneOffset();
   $scope.adjustHours=function(adj){
     var nowDate=new Date();
-    var UTChours=nowDate.getUTCHours();
-    nowDate.setHours(UTChours+adj);
+    var UTCHours=nowDate.getUTCHours();
+    var UTCMinutes=nowDate.getUTCMinutes();
+    nowDate.setHours(UTCHours);
+    nowDate.setMinutes(UTCMinutes+(adj*60));
     return nowDate;
   }
   $scope.offsetObj={};
   $scope.dateOutput=nowDate;
-  $scope.addSecond=function(dateIn){
-    var current=dateIn.getSeconds();
-    dateIn.setSeconds(current+1);
-  }
   $scope.setIt=function(){
-    if ($scope.offsetObj.abbr!="COMP"){
+    if (!$scope.offsetObj){
+      var d=new Date();
+      var n=d.getTimezoneOffset();
+      $scope.thisIsTheOffset=(-n/60);
+    }
+    else if ($scope.offsetObj.abbr!="COMP"){
       $scope.thisIsTheOffset=$scope.offsetObj.offset||0;
     }
     else {
@@ -47,7 +71,14 @@ var thisApp=angular.module('time_finder',[])
       $scope.thisIsTheOffset=(-n/60);
     }
     $scope.dateOutput=$scope.adjustHours($scope.thisIsTheOffset);
-    $scope.test=$scope.offsetObj.text;
   }
-  $interval(function(){$scope.addSecond($scope.dateOutput)},1000);
+  $scope.changeIt=function(){
+    $scope.setIt();
+    if (!$scope.offsetObj){
+      $scope.test="Your time: "
+    }
+    else {
+      $scope.test=$scope.offsetObj.text;}
+  }
+  $interval(function(){$scope.setIt()},1000);
 }])
